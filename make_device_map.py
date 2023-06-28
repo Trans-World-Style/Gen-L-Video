592eac2ca4c0315fd7b9195bdc04f642198532d1
강두원 <kangdoowon@gangduwon-ui-MacBookPro.local>
2023-06-28 23:34:38 +0900

    check for distributed

--- make_device_map.py
+++ make_device_map.py
@@ -8,10 +8,10 @@ pretrained_model_path = "weights/anything-v4.0"
 # noise_scheduler = DDPMScheduler.from_pretrained(pretrained_model_path, subfolder="scheduler")
 # device_map = infer_auto_device_map(noise_scheduler, max_memory={0: "16GiB", 1: "16GiB", "cpu": "24GiB"})
 # print(device_map)
-
-tokenizer = CLIPTokenizer.from_pretrained(pretrained_model_path, subfolder="tokenizer")
-device_map = infer_auto_device_map(tokenizer, max_memory={0: "16GiB", 1: "16GiB", "cpu": "24GiB"})
-print(device_map)
+#
+# tokenizer = CLIPTokenizer.from_pretrained(pretrained_model_path, subfolder="tokenizer")
+# device_map = infer_auto_device_map(tokenizer, max_memory={0: "16GiB", 1: "16GiB", "cpu": "24GiB"})
+# print(device_map)
 
 text_encoder = CLIPTextModel.from_pretrained(pretrained_model_path, subfolder="text_encoder")
 device_map = infer_auto_device_map(text_encoder, max_memory={0: "16GiB", 1: "16GiB", "cpu": "24GiB"})
