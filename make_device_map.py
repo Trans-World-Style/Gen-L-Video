0ef96994fd93f9e199fa566621274d1dd30fce43
강두원 <kangdoowon@gangduwon-ui-MacBookPro.local>
2023-06-28 23:37:53 +0900

    check for distributed

--- make_device_map.py
+++ make_device_map.py
@@ -2,26 +2,18 @@ from diffusers import DDPMScheduler, AutoencoderKL
 from transformers import CLIPTokenizer, CLIPTextModel
 from glv.models.unet import UNet3DConditionModel
 from accelerate import infer_auto_device_map
+from accelerate import init_empty_weights
 
 pretrained_model_path = "weights/anything-v4.0"
 
-# noise_scheduler = DDPMScheduler.from_pretrained(pretrained_model_path, subfolder="scheduler")
-# device_map = infer_auto_device_map(noise_scheduler, max_memory={0: "16GiB", 1: "16GiB", "cpu": "24GiB"})
-# print(device_map)
-#
-# tokenizer = CLIPTokenizer.from_pretrained(pretrained_model_path, subfolder="tokenizer")
-# device_map = infer_auto_device_map(tokenizer, max_memory={0: "16GiB", 1: "16GiB", "cpu": "24GiB"})
-# print(device_map)
 
-# text_encoder = CLIPTextModel.from_pretrained(pretrained_model_path, subfolder="text_encoder")
-# device_map = infer_auto_device_map(text_encoder, max_memory={0: "16GiB", 1: "16GiB", "cpu": "24GiB"})
-# print(device_map)
+with init_empty_weights():
+    vae = AutoencoderKL.from_pretrained(pretrained_model_path, subfolder="vae")
+    device_map = infer_auto_device_map(vae, max_memory={0: "16GiB", 1: "16GiB", "cpu": "24GiB"})
+    print(device_map)
 
-vae = AutoencoderKL.from_pretrained(pretrained_model_path, subfolder="vae")
-device_map = infer_auto_device_map(vae, max_memory={0: "16GiB", 1: "16GiB", "cpu": "24GiB"})
-print(device_map)
-
-unet = UNet3DConditionModel.from_pretrained_2d(pretrained_model_path, subfolder="unet")
-device_map = infer_auto_device_map(unet, max_memory={0: "16GiB", 1: "16GiB", "cpu": "24GiB"})
-print(device_map)
+with init_empty_weights():
+    unet = UNet3DConditionModel.from_pretrained_2d(pretrained_model_path, subfolder="unet")
+    device_map = infer_auto_device_map(unet, max_memory={0: "16GiB", 1: "16GiB", "cpu": "24GiB"})
+    print(device_map)
 
