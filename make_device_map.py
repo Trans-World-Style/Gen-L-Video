7c473040731b85266e40abafe967e2b8eece669c
강두원 <kangdoowon@gangduwon-ui-MacBookPro.local>
2023-06-30 19:42:09 +0900

    check for distributed

--- make_device_map.py
+++ make_device_map.py
@@ -4,16 +4,22 @@ from glv.models.unet import UNet3DConditionModel
 from accelerate import infer_auto_device_map
 from accelerate import init_empty_weights
 
-pretrained_model_path = "weights/anything-v4.0"
+# pretrained_model_path = "weights/anything-v4.0"
+pretrained_model_path = "xyn-ai/anything-v4.0"
 
+from transformers import AutoConfig, AutoModelForCausalLM
+
+config = AutoConfig.from_pretrained(pretrained_model_path)
 
 with init_empty_weights():
-    vae = AutoencoderKL.from_pretrained(pretrained_model_path, subfolder="vae")
+    vae = AutoModelForCausalLM.from_config(config)
+
+    # vae = AutoencoderKL.from_pretrained(pretrained_model_path, subfolder="vae")
     device_map = infer_auto_device_map(vae, max_memory={0: "8GiB", 1: "8GiB", "cpu": "24GiB"})
     print(device_map)
 
-with init_empty_weights():
-    unet = UNet3DConditionModel.from_pretrained_2d(pretrained_model_path, subfolder="unet")
-    device_map = infer_auto_device_map(unet, max_memory={0: "8GiB", 1: "8GiB", "cpu": "24GiB"})
-    print(device_map)
+# with init_empty_weights():
+#     unet = UNet3DConditionModel.from_pretrained_2d(pretrained_model_path, subfolder="unet")
+#     device_map = infer_auto_device_map(unet, max_memory={0: "8GiB", 1: "8GiB", "cpu": "24GiB"})
+#     print(device_map)
 
