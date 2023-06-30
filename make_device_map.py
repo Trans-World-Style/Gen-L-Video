37e290b5730da97e29f8f0aec8f8f68c036de269
강두원 <kangdoowon@gangduwon-ui-MacBookPro.local>
2023-06-30 19:57:39 +0900

    check for distributed

--- make_device_map.py
+++ make_device_map.py
@@ -12,16 +12,16 @@ pretrained_model_path = "./weights/anything-v4.0"
 from transformers import AutoConfig, AutoModelForCausalLM
 
 # config = AutoConfig.from_pretrained(f'{pretrained_model_path}/vae/config.json')
-# config = AutoConfig.from_pretrained(f'{pretrained_model_path}', subfolder='vae')
+config = AutoConfig.from_pretrained(f'{pretrained_model_path}', subfolder='vae')
 
 
 with init_empty_weights():
-    # vae = AutoModelForCausalLM.from_config(config)
-    vae = AutoencoderKL.from_pretrained(pretrained_model_path, subfolder="vae")
-    vae = load_checkpoint_and_dispatch(vae, device_map='auto')
-    print(vae.hf_device_map)
-    # device_map = infer_auto_device_map(vae, max_memory={0: "8GiB", 1: "8GiB", "cpu": "24GiB"})
-    # print(device_map)
+    vae = AutoModelForCausalLM.from_config(config)
+    # vae = AutoencoderKL.from_pretrained(pretrained_model_path, subfolder="vae")
+    # vae = load_checkpoint_and_dispatch(vae, device_map='auto')
+    # print(vae.hf_device_map)
+    device_map = infer_auto_device_map(vae, max_memory={0: "8GiB", 1: "8GiB", "cpu": "24GiB"})
+    print(device_map)
 
 # config = AutoConfig.from_pretrained(f'{pretrained_model_path}', subfolder='unet')
 # with init_empty_weights():
