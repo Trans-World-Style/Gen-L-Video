670f0429f08360246f2d0617e4b0c6b2dbc248aa
강두원 <kangdoowon@gangduwon-ui-MacBookPro.local>
2023-07-01 12:12:55 +0900

    check for distributed

--- one-shot-tuning.py
+++ one-shot-tuning.py
@@ -374,9 +374,9 @@ def main(
                 pixel_values = batch["pixel_values"].to(weight_dtype)
                 video_length = pixel_values.shape[1]
                 pixel_values = rearrange(pixel_values, "b f c h w -> (b f) c h w")
-                to_cuda(1, [vae, pixel_values])
                 print(vae.device)
                 print(pixel_values.device)
+                vae, pixel_values = to_cuda(1, [vae, pixel_values])
                 latents = vae.encode(pixel_values).latent_dist.sample()
                 vae, pixel_values = to_cuda(0, [vae, pixel_values])
                 check_gpu('encode')
