952010972d2640b8c438f07ac681f8fe5f557ae3
강두원 <kangdoowon@gangduwon-ui-MacBookPro.local>
2023-07-01 14:03:03 +0900

    check for distributed

--- one-shot-tuning.py
+++ one-shot-tuning.py
@@ -178,7 +178,6 @@ def main(
     print('%% unet loaded %%')
 
     unet.to(accelerator.device, dtype=torch.float16)
-    tokenizer.to(accelerator.device, dtype=torch.float16)
     text_encoder.to(accelerator.device, dtype=torch.float16)
     vae.to(accelerator.device, dtype=torch.float16)
     check_gpu('hihi')
