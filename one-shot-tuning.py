13e360a67a5b3be947a126aff8a7e04a61782273
강두원 <kangdoowon@gangduwon-ui-MacBookPro.local>
2023-07-01 12:38:29 +0900

    check for distributed

--- one-shot-tuning.py
+++ one-shot-tuning.py
@@ -166,16 +166,19 @@ def main(
     device_map = 'balanced_low_0'
     # device_map = 'sequential'
     # Load scheduler, tokenizer and models.
-    noise_scheduler = DDPMScheduler.from_pretrained(pretrained_model_path, subfolder="scheduler", device_map=device_map)
-    print('%% scheduler loaded %%')
-    tokenizer = CLIPTokenizer.from_pretrained(pretrained_model_path, subfolder="tokenizer", device_map=device_map)
-    print('%% tokenizer loaded %%')
-    text_encoder = CLIPTextModel.from_pretrained(pretrained_model_path, subfolder="text_encoder", device_map=device_map)
-    print('%% text_encoder loaded %%')
+    # noise_scheduler = DDPMScheduler.from_pretrained(pretrained_model_path, subfolder="scheduler", device_map=device_map)
+    # print('%% scheduler loaded %%')
+    # tokenizer = CLIPTokenizer.from_pretrained(pretrained_model_path, subfolder="tokenizer", device_map=device_map)
+    # print('%% tokenizer loaded %%')
+    # text_encoder = CLIPTextModel.from_pretrained(pretrained_model_path, subfolder="text_encoder", device_map=device_map)
+    # print('%% text_encoder loaded %%')
     vae = AutoencoderKL.from_pretrained(pretrained_model_path, subfolder="vae", device_map=device_map)
     print('%% vae loaded %%')
-    unet = UNet3DConditionModel.from_pretrained_2d(pretrained_model_path, subfolder="unet", device_map=device_map)
-    print('%% unet loaded %%')
+    vae.to(accelerator.device, dtype='f16')
+    check_gpu('hihi')
+    print(vae.device)
+    # unet = UNet3DConditionModel.from_pretrained_2d(pretrained_model_path, subfolder="unet", device_map=device_map)
+    # print('%% unet loaded %%')
 
 
     if adapter_path is not None:
