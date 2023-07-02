5ead74ec5386742f2877323790b070e7089fedd8
강두원 <kangdoowon@gangduwon-ui-MacBookPro.local>
2023-07-02 19:23:48 +0900

    check for distributed

--- one-shot-tuning.py
+++ one-shot-tuning.py
@@ -376,8 +376,8 @@ def main(
         unet.train()
         train_loss = 0.0
         for step, batch in enumerate(train_dataloader):
-            for bk, bv in batch.items():
-                print(f'{bk}: {bv.dtype}')
+            # for bk, bv in batch.items():
+            #     print(f'{bk}: {bv.dtype}')
             check_gpu('in step')
             # Skip steps until we reach the resumed step
             if resume_from_checkpoint and epoch == first_epoch and step < resume_step:
@@ -391,6 +391,7 @@ def main(
                 pixel_values = batch["pixel_values"].to(weight_dtype)
                 video_length = pixel_values.shape[1]
                 pixel_values = rearrange(pixel_values, "b f c h w -> (b f) c h w")
+                print(f'pixel : {pixel_values.dtype}')
                 latents = vae.encode(pixel_values).latent_dist.sample()
                 latents = rearrange(latents, "(b f) c h w -> b c f h w", f=video_length)
                 latents = latents * 0.18215
