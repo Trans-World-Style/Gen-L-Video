4f61e357d42da62554cc363f6c41444337d3b8ac
강두원 <kangdoowon@gangduwon-ui-MacBookPro.local>
2023-07-02 21:02:31 +0900

    check for distributed

--- one-shot-tuning.py
+++ one-shot-tuning.py
@@ -437,7 +437,7 @@ def main(
                 if control is not None:
                     control= rearrange(control, "b f c h w -> b c f h w").to(weight_dtype)
                 ######################
-                def wrapper_fn(noisy_latents, timesteps, clip_id, encoder_hidden_states, control):
+                def wrapper_fn(noisy_latents, timesteps, clip_id, encoder_hidden_states, control=control):
                     y = unet(noisy_latents, timesteps, clip_id, encoder_hidden_states, control=control).sample
                     return y
                 pred_fn = torch._dynamo.optimize(wrapper_fn)
