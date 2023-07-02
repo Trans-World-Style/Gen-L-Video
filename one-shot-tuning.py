9c190d53bcfa0dfabec8fcf1761bc61bff163858
강두원 <kangdoowon@gangduwon-ui-MacBookPro.local>
2023-07-02 19:38:47 +0900

    check for distributed

--- one-shot-tuning.py
+++ one-shot-tuning.py
@@ -391,7 +391,7 @@ def main(
                 pixel_values = batch["pixel_values"].to(weight_dtype)
                 video_length = pixel_values.shape[1]
                 pixel_values = rearrange(pixel_values, "b f c h w -> (b f) c h w")
-                pixel_values.to(vae.device)
+                pixel_values = pixel_values.to(vae.device)
                 latents = vae.encode(pixel_values).latent_dist.sample()
                 latents = rearrange(latents, "(b f) c h w -> b c f h w", f=video_length)
                 latents = latents * 0.18215
