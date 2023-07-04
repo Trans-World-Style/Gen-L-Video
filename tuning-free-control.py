c590430e4097e9b72dd4237dc2b654830b67de3a
강두원 <kangdoowon@gangduwon-ui-MacBookPro.local>
2023-07-04 18:26:34 +0900

    check for distributed

--- tuning-free-control.py
+++ tuning-free-control.py
@@ -185,8 +185,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--config", type=str, default="./configs/tuning-free-control/girl-glass.yaml")
     args = parser.parse_args()
-    # args.config.n_sample_frames = 1
 
-    main(**OmegaConf.load(args.config))
+    config = OmegaConf.load(args.config)
+    config['n_sample_frames'] = 1
+    main(**config)
     max_memory_allocated = torch.cuda.max_memory_allocated() / (1024 ** 3) 
     print(f"max memory allocated: {max_memory_allocated:.3f} GB.")
