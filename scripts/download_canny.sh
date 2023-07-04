371a8d871af893c0459650ec5c77bb0fb8778b08
강두원 <kangdoowon@gangduwon-ui-MacBookPro.local>
2023-07-04 15:42:46 +0900

    check for distributed

--- scripts/download_canny.sh
+++ scripts/download_canny.sh
@@ -1,3 +1,7 @@
 cd weights
 
 git clone https://huggingface.co/lllyasviel/sd-controlnet-canny
+rm -rf ./sd-controlnet-canny/**/*.bin
+rm -rf ./sd-controlnet-canny/**/*.safetensors
+wget -P ./sd-controlnet-canny/ https://huggingface.co/lllyasviel/sd-controlnet-canny/resolve/main/diffusion_pytorch_model.safetensors
+wget -P ./sd-controlnet-canny/ https://huggingface.co/lllyasviel/sd-controlnet-canny/resolve/main/diffusion_pytorch_model.bin
