8945f906015cf75f528476e455b190f84a0ad9aa
강두원 <kangdoowon@gangduwon-ui-MacBookPro.local>
2023-07-05 19:42:20 +0900

    check for distributed

--- tuning-free-control.py
+++ tuning-free-control.py
@@ -170,7 +170,6 @@ def main(
             pixel_values = rearrange(pixel_values, "b f c h w -> (b f) c h w")
             latents = [ ]
             for i in range(0,video_length,validation_data.video_length):
-                check_gpu()
                 latents.append( vae.encode(pixel_values[i:i+validation_data.video_length].to(vae.device)).latent_dist.sample())
             latents = torch.cat(latents,dim=0)
             latents = rearrange(latents, "(b f) c h w -> b c f h w", f=video_length)
